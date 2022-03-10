1caefeec103097ab69eef0d60b446c66afe4d280
Amruth <amruth_31393@yahoo.in>
2022-03-09 22:05:44 -0500

    my thesis code changes

--- Header Files/math_functions.hpp
+++ Header Files/math_functions.hpp
@@ -154,12 +154,15 @@ template <typename Dtype>
 void caffe_gpu_gemm(const CBLAS_TRANSPOSE TransA,
     const CBLAS_TRANSPOSE TransB, const int M, const int N, const int K,
     const Dtype alpha, const Dtype* A, const Dtype* B, const Dtype beta,
-    Dtype* C);
+    Dtype* C, float* pruning, bool is_normal);
 
 template <typename Dtype>
 void caffe_gpu_gemv(const CBLAS_TRANSPOSE TransA, const int M, const int N,
     const Dtype alpha, const Dtype* A, const Dtype* x, const Dtype beta,
     Dtype* y);
+
+template <typename Dtype>
+void test_conv_function(const Dtype* weights, const Dtype* inputs, Dtype* outputs,int M, int N, int K, float* pruning, bool is_normal);
 
 template <typename Dtype>
 void caffe_gpu_axpy(const int N, const Dtype alpha, const Dtype* X,
